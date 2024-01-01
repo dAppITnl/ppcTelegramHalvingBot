50ba73c5373771c2a83c40ba79d3d17cf67b0ca9
Cas Knook <nwkcoins@gmail.com>
2024-01-01 22:57:17 +0100

    bash script to run from crontab

---
+++ runhalving.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to the specified directory
+cd /home/serverw3/vhost/bot-support.com/pinkpanthercapital.bot-support.com/ppcTelegramHalvingBot
+
+# Activate the virtual environment
+source env/bin/activate
+
+# Run the Python script and redirect output to log.file
+/usr/bin/python3 ppchalving_countdown.py >> ppchalving_countdown.log
